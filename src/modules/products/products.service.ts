89ded9b76ee5671f46f85e79c059b867b8845c4c
FelipeDasr <felipe.heiker07@gmail.com>
2022-05-20 17:12:38 -0300

    New method (create)

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, Injectable, } from "@nestjs/common";
+
+import { CreateProductDTO, ProductDTO } from "src/dtos/product.dto";
 
 import { PrismaService } from "../../database/prisma.service";
-import { Product, Prisma } from "@prisma/client";
 
 @Injectable()
 export class ProductsService {
-    constructor(private readonly productService: PrismaService) {}
+    constructor(private readonly prisma: PrismaService) { }
+
+    async create(data: CreateProductDTO): Promise<ProductDTO> {
+        // Gets a product with the same name
+        const productAlreadyExists = await this.prisma.product.findFirst({
+            where: {
+                name: data.name
+            }
+        });
+
+        // Check if the product already exists
+        if (productAlreadyExists) {
+            throw new HttpException(`Product already exists`, 400);
+        }
+
+        // Create the new product
+        return await this.prisma.product.create({ data });
+    }
+
+    
 }
