130a6da6ccea453b960ba309a5960c1aa9d63e9f
FelipeDasr <felipe.heiker07@gmail.com>
2022-05-22 23:16:13 -0300

    New method (delete)

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, Injectable, } from "@nestjs/common";
 
-import { CreateProductDTO, ProductDTO } from "src/dtos/product.dto";
+import { CreateProductDTO, DeletedProductDTO, ProductDTO } from "src/dtos/product.dto";
 
 import { PrismaService } from "../../database/prisma.service";
 
@@ -25,5 +25,20 @@ export class ProductsService {
         return await this.prisma.product.create({ data });
     }
 
-    
+    async delete(id: string): Promise<DeletedProductDTO> {
+        
+        const product = await this.prisma.product.findFirst({
+            where: {
+                id
+            }
+        });
+
+        // Check if the product exists
+        if (!product) {
+            throw new HttpException(`Product does not exist`, 400);
+        }
+
+        const deletedProduct: ProductDTO = await this.prisma.product.delete({ where: { id } });
+        return { deletedProduct }
+    }
 }
