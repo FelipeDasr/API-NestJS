bb1bcde1d23db4283002f1dd810c9ab78e7e99d8
FelipeDasr <felipe.heiker07@gmail.com>
2022-05-22 22:02:19 -0300

    New controller added (create) and modifications to Contructor method

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+import { CreateProductDTO } from 'src/dtos/product.dto';
 
 @Controller()
 export class ProductsController {
 
-    @Get()
-    index(){
-        return 'hello world';
+    constructor(private readonly productServices: ProductsService){}
+
+    @Post('create')
+    async create(@Body() productData: CreateProductDTO){
+        return await this.productServices.create(productData);
     }
 }
