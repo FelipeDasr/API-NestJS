f08bef58d56035ea2fd4926fd17615734fa5c91b
FelipeDasr <felipe.heiker07@gmail.com>
2022-05-24 13:19:29 -0300

    Formatted document

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,11 +1,12 @@
-import { 
-    ParseUUIDPipe, 
-    Controller, 
-    Delete, 
-    Param, 
-    Body, 
+import {
+    ParseUUIDPipe,
+    Controller,
+    Delete,
+    Param,
+    Body,
     Post,
-    Get, 
+    Get,
+    Query,
 } from '@nestjs/common';
 
 import { ProductsService } from './products.service';
@@ -15,20 +16,20 @@ import { CreateProductDTO, ProductDTO } from 'src/dtos/product.dto';
 @Controller()
 export class ProductsController {
 
-    constructor(private readonly productServices: ProductsService){}
+    constructor(private readonly productServices: ProductsService) { }
 
     @Post('create')
-    async create(@Body() productData: CreateProductDTO): Promise<ProductDTO>{
+    async create(@Body() productData: CreateProductDTO): Promise<ProductDTO> {
         return await this.productServices.create(productData);
     }
 
     @Get('product/:id')
-    async getProduct(@Param('id', ParseUUIDPipe) id: string){
+    async getProduct(@Param('id', ParseUUIDPipe) id: string) {
         return await this.productServices.getProductById(id);
     }
 
     @Delete('delete/:id')
-    async delete(@Param('id', ParseUUIDPipe) id: string){
+    async delete(@Param('id', ParseUUIDPipe) id: string) {
         return await this.productServices.delete(id);
     }
 }
