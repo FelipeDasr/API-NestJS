37495219a8e82d5f4aab645199c09107f0836139
FelipeDasr <felipe.heiker07@gmail.com>
2022-05-30 12:56:55 -0300

    Uploads configs added

---
+++ src/configs/index.ts
@@ -0,0 +1,34 @@
+import {
+    MulterOptions
+} from "@nestjs/platform-express/multer/interfaces/multer-options.interface";
+
+import { HttpException, ValidationPipeOptions } from "@nestjs/common";
+import { isImageFile, generateFilename } from '../utils';
+import { diskStorage } from 'multer';
+
+export const validationPipeOptions: ValidationPipeOptions = {
+    whitelist: true,
+    transform: true,
+    forbidUnknownValues: true
+}
+
+export const uploadConfig: MulterOptions = {
+    limits: {
+        fileSize: 1048576, // 1MB
+        files: 5,
+    },
+    storage: diskStorage({
+        filename: (req, file, cb) => {
+            return cb(null, generateFilename(file.originalname));
+        }
+    }),
+    fileFilter: (req, file, cb) => {
+        if (isImageFile(file.originalname)) {
+            return cb(null, true);
+        }
+        return cb(
+            new HttpException('Only image files are allowed!', 400),
+            false
+        );
+    },
+}
