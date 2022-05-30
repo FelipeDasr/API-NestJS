16a727ce4392730c761d2dead9db0b9b160353b1
FelipeDasr <felipe.heiker07@gmail.com>
2022-05-30 12:50:52 -0300

    Add a new provider to products module (PhotosService)

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
+import { PhotosService } from '../photos/photos.service';
 
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
     controllers: [ProductsController],
-    providers: [ProductsService, PrismaService],
+    providers: [
+        ProductsService,
+        PhotosService,
+        PrismaService,
+    ],
 })
-export class ProductsModule {}
+export class ProductsModule { }
