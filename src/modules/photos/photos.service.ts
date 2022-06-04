ce3d4e98a87711db9b92882be73fd49cb9ea7b30
FelipeDasr <felipe.heiker07@gmail.com>
2022-06-04 16:15:21 -0300

    New service (uploadPhoto)

--- src/modules/photos/photos.service.ts
+++ src/modules/photos/photos.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/database/prisma.service";
 
+import { generateFilename } from "src/utils";
+import { S3 } from "aws-sdk";
+import { CreatePhotoDTO } from "src/dtos/photo.dto";
+
 @Injectable({})
 export class PhotosService {
 
-    constructor(private readonly prisma: PrismaService) { }
+    private s3: S3;
 
+    constructor(private readonly prisma: PrismaService) {
+        this.s3 = new S3();
+    }
+
+    async uploadPhoto(photo: Express.Multer.File) {
+        // Upload parameters
+        const parameters = {
+            Bucket: process.env.BUCKET_NAME,
+            Body: photo.buffer,
+            Key: generateFilename(photo.originalname),
+            ContentType: photo.mimetype,
+            ACL: 'public-read'
+        }
+
+        // Upload file
+        const res = await this.s3.upload(parameters).promise();
+
+        // Return photo data
+        return { key: res.Key, url: res.Location }
+    }
 }
